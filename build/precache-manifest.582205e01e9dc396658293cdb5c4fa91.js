3d5ad80da80487aac9377ddc54bddf3b2a1a20ae
alexisjcarr <acarr59@gmail.com>
2019-08-18 10:39:13 -0500

    🔥 MVP
     #1

---
+++ build/precache-manifest.582205e01e9dc396658293cdb5c4fa91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51da1435c957acc1e681843f338a1ca9",
+    "url": "/tinder-interface/index.html"
+  },
+  {
+    "revision": "d0f82132b71bb5c018d3",
+    "url": "/tinder-interface/static/css/2.54346816.chunk.css"
+  },
+  {
+    "revision": "0fd2a138cc14a6e6d5c3",
+    "url": "/tinder-interface/static/css/main.750fbbf3.chunk.css"
+  },
+  {
+    "revision": "d0f82132b71bb5c018d3",
+    "url": "/tinder-interface/static/js/2.173c1fbe.chunk.js"
+  },
+  {
+    "revision": "0fd2a138cc14a6e6d5c3",
+    "url": "/tinder-interface/static/js/main.2218d7cd.chunk.js"
+  },
+  {
+    "revision": "36fc5b068cc1fa2777b8",
+    "url": "/tinder-interface/static/js/runtime~main.440cd2c7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/tinder-interface/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/tinder-interface/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/tinder-interface/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/tinder-interface/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/tinder-interface/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
