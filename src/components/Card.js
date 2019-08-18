b250be16f4ad46bb2fd72bc0a2258eb170fe0922
alexisjcarr <acarr59@gmail.com>
2019-08-18 09:39:07 -0500

    🔥 MVP almost complete
     #1

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
-import interact from 'interactjs'
-import TWEEN from '@tweenjs/tween.js'
+import React, { Component } from "react";
+import interact from "interactjs";
+import TWEEN from "@tweenjs/tween.js";
+
+import axios from "axios";
 
 function animate(time) {
   requestAnimationFrame(animate);
@@ -9,59 +11,71 @@ function animate(time) {
 
 export default class Card extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       x: 0,
       y: 0
-    }
+    };
   }
 
   componentDidMount() {
-    let inter = interact('#card' + this.props.idx)
+    let inter = interact("#card" + this.props.idx);
     inter.draggable({
       inertia: true,
       onmove: this.handleDrag.bind(this),
       onend: this.handleDragEnd.bind(this)
-    })
+    });
 
     requestAnimationFrame(animate);
   }
 
-  handleDrag (event) {
-     var x = (parseFloat(this.state.x) || 0) + event.dx,
-         y = (parseFloat(this.state.y) || 0) + event.dy;
-     // update the posiion attributes
-     this.setState({x, y})
-   }
+  handleDrag(event) {
+    var x = (parseFloat(this.state.x) || 0) + event.dx,
+      y = (parseFloat(this.state.y) || 0) + event.dy;
+    // update the posiion attributes
+    this.setState({ x, y });
+  }
 
   handleDragEnd(event) {
     let positionX = event.pageX;
-    let card = this
-    let leftBound = -50
-    let rightBound = window.innerWidth + 50
+    let card = this;
+    let leftBound = -50;
+    let rightBound = window.innerWidth + 50;
 
     if (positionX < rightBound && positionX > leftBound) {
-      var tween = new TWEEN.Tween({x: this.state.x, y: this.state.y})
-      tween.to({ x: 0, y: 0 }, 250)
-      tween.onUpdate(function () {
-        card.setState({x: this.x, y: this.y})
-      })
+      var tween = new TWEEN.Tween({ x: this.state.x, y: this.state.y });
+      tween.to({ x: 0, y: 0 }, 250);
+      tween.onUpdate(function() {
+        card.setState({ x: this.x, y: this.y });
+      });
       tween.start();
     } else if (positionX > rightBound) {
-      this.props.shiftCard()
+      this.props.shiftCard();
+      axios
+        .post("http://localhost:5000/api/events/3", {
+          user_id: 3
+        })
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     } else if (positionX < leftBound) {
-      this.props.shiftCard()
+      this.props.shiftCard();
     }
   }
 
   render() {
-    let {x, y} = this.state
+    let { x, y } = this.state;
     let cardStyle = {
-        transform: 'translate(' + x + 'px, ' + y + 'px)',
-    }
+      transform: "translate(" + x + "px, " + y + "px)"
+    };
 
-    return <div id={"card"+ this.props.idx} className="Card" style={cardStyle} >
-      <img src={process.env.PUBLIC_URL + this.props.imgUrl} alt="me" />
-    </div>
+    return (
+      <div id={"card" + this.props.idx} className="Card" style={cardStyle}>
+        <img src={process.env.PUBLIC_URL + this.props.imgUrl} alt="me" />
+      </div>
+    );
   }
 }
